75fa997b44253e469c879afd20139d93a151e4d5
Syed M Tayyab <81333851+syedmtayyabsmt@users.noreply.github.com>
2023-04-17 13:12:32 +0500

    Update Python Basics.py

--- Python Basics.py
+++ Python Basics.py
@@ -1,3 +1,35 @@
+# ########################################################################################################################################
+# Check Uppercase, Lowercase & Numbers In String
+def string_and_number_calculator(string):
+    checker_dict = {'Lower_case': 0, 'Upper_case': 0, 'Number': 0}
+
+    for letter in string:
+        if letter.isupper():
+            checker_dict['Upper_case'] = checker_dict['Upper_case'] + 1
+
+        elif letter.islower():
+            checker_dict['Lower_case'] = checker_dict['Lower_case'] + 1
+
+        elif letter.isnumeric():
+            checker_dict['Number'] = checker_dict['Number'] + 1
+
+        else:
+            pass
+
+    print(f"There Are {checker_dict['Upper_case']} Upper Case In Text")
+    print(f"There Are {checker_dict['Lower_case']} Lower Case In Text")
+    print(f"There Are {checker_dict['Number']} Numbers In Text")
+
+
+i_string = input('Write Text To Check :: ')
+
+string_and_number_calculator(i_string)
+
+# ############################################################
+# 
+
+# ########################################################################################################################################
+
 # TAKING INPUT
 # We Can Use Both Single And Double Quotes In Print Statement For Writing Strings
 
