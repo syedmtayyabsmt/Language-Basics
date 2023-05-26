e4812ed406a5469daaa48df4f453ed0d3dcbe37f
Syed M Tayyab <81333851+syedmtayyabsmt@users.noreply.github.com>
2023-05-26 10:04:35 +0500

    Update Python Basics.py

--- Python Basics.py
+++ Python Basics.py
@@ -119,7 +119,7 @@ print(f'Prime Numbers Under {value} Are: {prime_nums}')
 
 # ############################################################
 # ############################################################
-# QR Code Reader
+# QR Code Reader Of Text
 
 import cv2 as cv
 import time
@@ -163,6 +163,30 @@ while True:
             print(f'Your Total Bill Is {total}')
 
         continue
+
+    cv.imshow('Preview', img)
+    if cv.waitKey(1) == 27:
+        break
+        
+ 
+# ############################################################
+# ############################################################
+# QR Code Reader Of Link (Link Opener)
+
+import cv2 as cv
+import webbrowser
+
+camera = cv.VideoCapture(0)
+
+while True:
+    success, img = camera.read()
+    qrd = cv.QRCodeDetector()
+    a, b, c = qrd.detectAndDecode(img)
+
+    if a != '':
+        print(a)
+        webbrowser.open(a)
+        break
 
     cv.imshow('Preview', img)
     if cv.waitKey(1) == 27:
