2f993ab6410a4714219feca22b3e7b801e4775af
Syed M Tayyab <81333851+syedmtayyabsmt@users.noreply.github.com>
2023-06-03 16:00:12 +0500

    Update Python Basics.py

--- Python Basics.py
+++ Python Basics.py
@@ -73,15 +73,13 @@ elif (gross_salary <= 50000):
 # ############################################################
 # Net Salary Calculator With Dictionary And Exception Handling
 
+import winsound as win
+
 employee_data = []
 
-while exit != 'Yes':
+while exit != 'Y':
 
-    try:   
-        id = int(input("Enter Employee's Id No: "))
-    except ValueError:
-        print('(ERROR) "Enter Number Only"')
-        continue
+    name = input("Enter Employee's Name: ")
 
     try:   
         basic_salary = int(input('Input Employee Basic Salary: '))
@@ -93,23 +91,50 @@ while exit != 'Yes':
     convence_allowance = (25 * basic_salary) / 100
     gross_salary = basic_salary + medical_allowance + convence_allowance
 
-    employee_data.append({'Employee Id': id,
+    if gross_salary > 50000:
+        tax_value = 4
+        income_tax = (tax_value*gross_salary)/100
+        net_salary = gross_salary - income_tax
+
+    elif gross_salary <= 50000:
+        tax_value = 2
+        income_tax = (tax_value*gross_salary)/100
+        net_salary = gross_salary - income_tax
+
+
+    employee_data.append({'Employee Name': name,
                          'Basic Salary': basic_salary,
                          'Medical Allowance': medical_allowance,
                          'Convence Allowance': convence_allowance,
-                         'Gross Salary': gross_salary})
+                         'Gross Salary': gross_salary,
+                         'Income Tax': income_tax,
+                         'Tax Value': tax_value,
+                         'Net Salary': net_salary})
 
     
-    exit = input('Do You Want To Exit ? (Enter "Yes" To Exit And "No" To Keep Going): ').capitalize()
-    if exit == 'Yes':
+    exit = input('Do You Want To Exit ? (Enter "Y" To Exit And "N" To Keep Going): ').capitalize()
+
+    if exit == 'Y':
         print('Program Ended')
+        win.Beep(1000,1000)
         break
-    elif exit == 'No':
+    
+    elif exit == 'N':
+        win.Beep(1000,300)
         continue
+    
     else:
-        while exit != 'Yes' and exit != 'No':
+        while exit != 'Y' and exit != 'N':
+
             print('Enter The Right Value')
             exit = input('Do You Want To Exit ? (Enter "Yes" To Exit And "No" To Keep Going): ').capitalize()
+            
+            if exit == 'Y':
+                print('Program Ended')
+
+            elif exit == 'N':
+                continue
+
 
 print('\n=====================')
 print("Data Of All Employees")
@@ -118,11 +143,13 @@ print('=====================\n')
 for i in employee_data:
 
     print(f'''
-Employee Id Is {i['Employee Id']}
+Employee Name Is {i['Employee Name']}
 Basic Salary Is {i['Basic Salary']}
 Medical Alllowance Is {i['Medical Allowance']}
 Convence Allowance Is {i['Convence Allowance']}
 Gross Salary Is {i['Gross Salary']}
+Income Tax On Salary Is {i['Income Tax']} Which Is {i['Tax Value']}%
+Your Net Salary Is {i['Net Salary']}
 ================================================
 ================================================''')
 
